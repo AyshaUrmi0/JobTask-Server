663bd3cd252d250e1d405281201d088e06ace6a6
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-03-04 23:35:06 +0600

    CRUD operation updated

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 
+
+app.use(cors());
+
+
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.jq7qb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.sth4y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,7 +24,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
+  }
 });
 
 async function run() {
@@ -93,6 +100,12 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+
+
+
+
+
 
 app.get("/", async (req, res) => {
   res.send("task management is running");
